5f2b828160e0a52c9dcce215f3220797a3c7496e
sevakogan <sevakogan@sevas-mbp.gateway.pace.com>
2015-12-19 11:54:12 -0800

    pushing back

--- Christmas Tree/TreePanel.java
+++ Christmas Tree/TreePanel.java
@@ -40,7 +40,7 @@ public class TreePanel extends JPanel implements ActionListener {
 	private int[] yArr = {TOP_Y, TOP_Y+80, TOP_Y+80, TOP_Y+160, TOP_Y+160, TOP_Y+240, 
 			TOP_Y+240, TOP_Y+160, TOP_Y+160, TOP_Y+80, TOP_Y+80};
 	private ArrayList<Ornament> orns;
-	private JButton blueButton = new JButton("Blue");
+	private JButton blueButton = new JButton("Yellow");
 	private JButton redButton = new JButton("Red");
 	private JButton startButton = new JButton("Start"); //button to start light show
 	private JButton stopButton = new JButton("Stop"); //button to stop light show
@@ -57,7 +57,7 @@ public class TreePanel extends JPanel implements ActionListener {
 		timer = new Timer(delay, this); //creates timer for the light show
 		
 	    blueButton.addActionListener(this);
-	    blueButton.setBackground(Color.blue);
+	    blueButton.setBackground(Color.yellow);
 	    blueButton.setOpaque(true);
 	    add(blueButton,BorderLayout.NORTH);
 	    
@@ -117,7 +117,7 @@ public class TreePanel extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent event) {
 		Object source = event.getSource();
 		if (source == blueButton){
-			clr = Color.blue;
+			clr = Color.yellow;
 		}
 		if (source == redButton){
 			clr = Color.red;
